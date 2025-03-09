a67d1d29a9973736af8d69ee1264244b4b6aabca
jaswanthreddy-007 <88081034+jaswanthreddy-007@users.noreply.github.com>
2025-03-10 02:53:58 +0530

    simple chart

--- hedge_lords_ui/src/app/components/trading/trading.component.ts
+++ hedge_lords_ui/src/app/components/trading/trading.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { MatGridListModule } from '@angular/material/grid-list';
+import { Component, OnDestroy } from '@angular/core';
 import { RestClientService } from '../../services/rest-client.service';
 import { Subscription } from 'rxjs';
 import { SettingsService } from '../../services/settings.service';
-import { HttpClient } from '@angular/common/http';
 import { OptionsChainComponent } from '../options-chain/options-chain.component';
 import { PositionAnalysisComponent } from '../position-analysis/position-analysis.component';
+import { PriceChartComponent } from '../price-chart/price-chart.component';
+import { HighchartsChartModule } from 'highcharts-angular';
 
 @Component({
   selector: 'app-trading',
   standalone: true,
-  imports: [OptionsChainComponent, PositionAnalysisComponent],
+  imports: [
+    OptionsChainComponent, 
+    PositionAnalysisComponent,
+    PriceChartComponent,
+    HighchartsChartModule
+  ],
   templateUrl: './trading.component.html',
   styleUrl: './trading.component.scss',
   providers: [SettingsService, RestClientService],
